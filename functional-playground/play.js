c87d6c75fe9d17a3c45a20f1092a22bb564d7fb4
vishwanathsrikanth <vism@microsoft.com>
2018-06-17 16:59:16 +0530

    updated redux sample

--- functional-playground/play.js
+++ functional-playground/play.js
@@ -1,7 +1,51 @@
-import forEach from '../lib/es6-functional.js'
+/********Integers in JS are  Immutable ******************* */
+var x = 5; // the state of x is 5 here 
+var y = x; // the state of y is same as that of x
 
-var array = [1,2,3]
-//print the console
-forEach(array,(data) => console.log(data))
-//double the contents
-forEach(array,(data) => console.log(2 * data))
+y = x * 2; // we are altering the state of y
+
+console.log('x = ' + x); // x is intact, pretty simple
+console.log('y = ' + y);
+
+/********Strings in JS are  Immutable ******************* */
+
+var x = 'Hello'; // the state of x is Hello here
+var y = x; // the state of y is same as x
+
+x = x +  ' World'; // altering the state of x
+
+console.log('x = ' + x); 
+console.log('y = ' + y); // y is intact
+
+/********Objects in JS are  Mutable ******************* */
+
+var x = { foo : 'Hello' };
+var y = x; // the state of y should be same as x
+
+x.foo +=  ' World'; // altering the state of x
+
+console.log('x = ' + x.foo);
+console.log('y = ' + y.foo); // y is also impacted
+console.log( x == y);
+
+/********Arrays in JS are  Mutable ******************* */
+
+var x = [ 'Red', 'Blue'];
+var y = x;
+
+x.push('Green');
+
+console.log('x = ' + x); // prints [ 'Red', 'Blue', 'Green' ]
+console.log('y = ' + y); // prints [ 'Red', 'Blue', 'Green' ]
+
+/********Forcing immutability on Objects ******************* */
+var x = { foo : 'Hello' };
+var y = x;
+
+Object.freeze(x);
+
+// y.foo +=  ' World'; -- uncomment this will throw error, both x and y are made read-only. 
+
+console.log('x = ' + x.foo);
+console.log('y = ' + y.foo);
+
